70a04a2f80ba72c2b7ca540f04ff24eb349cb7da
samlew89 <samclewis89@gmail.com>
2015-02-14 13:30:33 -0800

    Initial Commit

---
+++ RankMee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RankMee
+//
+//  Created by Sam Lewis on 2/14/15.
+//  Copyright (c) 2015 Sam Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
